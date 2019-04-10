7b294a7bb55fdf1b8c8ddc473712b00f36b490bd
surya-durgaputra <surya.durgaputra@gmail.com>
2019-04-10 17:01:36 +0400

    rnn

--- TFLrn/AmazonUploader.py
+++ TFLrn/AmazonUploader.py
@@ -7,7 +7,9 @@ Created on Tue Apr  9 12:45:05 2019
 
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
+################## MANUAL RNN #################################
 def manual_rnn_only_for_demo():
     #CONSTANTS
     num_inputs = 2
@@ -49,5 +51,66 @@ def manual_rnn_only_for_demo():
     here we showed only two timesteps.
     '''        
 
-manual_rnn_only_for_demo()
+#manual_rnn_only_for_demo()
     
+##############################################################
+
+## RNN WITH TF ###
+# target is to produce portions of a sine wave
+# based on the input and some labeled output,
+# our model is going to learn and then predict
+# values in the future. We know that the target
+# values are from sine wave (y_true) but the model
+# will learn this
+class TimeSeriesData():
+    def __init__(self,num_points,xmin,xmax):
+        self.xmin = xmin
+        self.xmax = xmax
+        self.num_points = num_points
+        self.resolution = (xmax-xmin)/num_points
+        self.x_data = np.linspace(xmin,xmax,num_points)
+        # the y_true is a sinusoid
+        self.y_true = np.sin(self.x_data)
+        
+    def ret_true(self,x_series):
+        return np.sin(x_series)
+    
+    #return_batch_ts means return_batch_timeseries
+    def next_batch(self,batch_size,steps,return_batch_ts=False):
+        # Grab a random starting point for each batch
+        rand_start = np.random.rand(batch_size,1)
+        # Convert to be on time series
+        ts_start = rand_start * (self.xmax - self.xmin - (steps*self.resolution))
+        # Create batch time series on the x-axis
+        batch_ts = ts_start + np.arange(0.0,steps+1) * self.resolution
+        print('rand_start:',rand_start)
+        print('ts_start:',ts_start)
+        print('resolution:',self.resolution)
+        print('batch_ts:',batch_ts)
+        # Create the Y data for the time series x axis from previous step
+        y_batch = np.sin(batch_ts)
+        # FORMATTING for RNN
+        if return_batch_ts:
+            return y_batch[:,:-1].reshape(-1,steps,1),y_batch[:,1:].reshape(-1,steps,1), batch_ts
+        else:
+            return y_batch[:,:-1].reshape(-1,steps,1),y_batch[:,1:].reshape(-1,steps,1)
+        
+        
+ts_data = TimeSeriesData(250,0,10)
+#plt.plot(ts_data.x_data,ts_data.y_true)
+num_time_steps = 30
+
+y1,y2,ts = ts_data.next_batch(1,num_time_steps,True)
+
+class Rect:
+    l = 9
+    def inisde(self):
+        print('attttr:',Rect.l)
+print('attr:',Rect.l)
+r = Rect()
+r.inisde()
+        
+        
+        
+        
+        
