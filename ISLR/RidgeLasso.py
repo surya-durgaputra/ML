182cc23637df14dfea9f833dc7244207001dc1f9
surya-durgaputra <surya.durgaputra@gmail.com>
2019-05-31 16:53:48 +0400

    added

--- ISLR/RidgeLasso.py
+++ ISLR/RidgeLasso.py
@@ -11,3 +11,30 @@ from sklearn import linear_model, utils
 import seaborn as sns
 
 df = pd.read_csv('D:/VS/Pt/ML/Keras/hitters.csv')
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn import datasets
+from sklearn.linear_model import LassoCV
+from sklearn.linear_model import Lasso
+from sklearn.model_selection import KFold
+from sklearn.model_selection import GridSearchCV
+
+from yellowbrick.regressor import AlphaSelection
+
+diabetes = datasets.load_diabetes()
+X = diabetes.data[:150]
+y = diabetes.target[:150]
+#X = diabetes.data
+#y = diabetes.target
+
+alphas = np.logspace(-5, -0.5, 30)
+
+# Instantiate the linear model and visualizer
+model = LassoCV(alphas=alphas)
+visualizer = AlphaSelection(model)
+
+visualizer.fit(X, y)
+g = visualizer.poof()
+
