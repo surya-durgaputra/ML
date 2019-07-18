80c0baf926679d769301360605c1f2c047c37b53
surya-durgaputra <surya.durgaputra@gmail.com>
2019-07-18 16:46:12 +0400

    ch3

---
+++ Alvaro/class.py
@@ -0,0 +1,10 @@
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+  def Hello():
+    print("No self")
